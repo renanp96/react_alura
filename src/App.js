5a2e80110b5a035f1b633a54a51b2837161b0ace
renanp96 <renanpa1996@gmail.com>
2022-03-28 11:36:18 -0300

    Manipulando o estado da aplicacao e padronizando o codigo

--- src/App.js
+++ src/App.js
@@ -10,12 +10,12 @@ class App extends Component {
 
     this.state = {
       notas: [],
-      categorias: [],
+      categorias: ["JoJo", "One Piece", "Fullmetal Alchemist"],
     };
   }
 
-  criarNota(titulo, texto) {
-    const novaNota = { titulo, texto };
+  criarNota(titulo, texto, categoria) {
+    const novaNota = { titulo, texto, categoria };
     const novoArrayNotas = [...this.state.notas, novaNota];
     const novoEstado = {
       notas: novoArrayNotas,
--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -6,6 +6,7 @@ class FormularioCadastro extends Component {
     super(props);
     this.titulo ="";
     this.texto ="";
+    this.categoria = "Sem Categoria";
   }
 
   _handleMudancaTitulo(evento){
@@ -18,21 +19,28 @@ class FormularioCadastro extends Component {
     this.texto = evento.target.value;
   }
 
+  _handleMudancaCategoria(evento){
+    evento.stopPropagation();
+    this.categoria = evento.target.value;
+  }
+
   _criarNota(evento){
     evento.preventDefault();
     evento.stopPropagation();
-    this.props.criarNota(this.titulo, this.texto);
+    this.props.criarNota(this.titulo, this.texto, this.categoria);
     
   }
 
   render() {
     return (
-      <form className="form-cadastro"
-        onSubmit={this._criarNota.bind(this)}
-      >
-        <select className="form-cadastro_input">
-          {this.props.categorias.map(categoria => {
-            return <option>{categoria}</option>
+      <form className="form-cadastro" onSubmit={this._criarNota.bind(this)}>
+        <select
+          onChange={this._handleMudancaCategoria.bind(this)}
+          className="form-cadastro_input"
+        >
+          <option>Sem Categoria</option>
+          {this.props.categorias.map((categoria) => {
+            return <option>{categoria}</option>;
           })}
         </select>
         <input
